963eba6ba4a78e549028f72f217df3658e923858
andreagurioli1995 <38469046+andreagurioli1995@users.noreply.github.com>
2020-11-05 15:38:39 +0100

    upd

--- nn.py
+++ nn.py
@@ -1,3 +1,4 @@
+import numpy as np
 class nn:
     def __init__(self,x):
         self.x=x
@@ -8,4 +9,20 @@ class nn:
 
 
 x= nn(1)
+
+#trasposta
+#training_outputs= np.array([[0,1,1,0]]).T
+
+#moltiplicazione riga per colonna
+#pesi_sinaptici+= np.dot(input_layer.T,adjustment)
+
+#random
+#np.random.seed(1)
+#pesi_sinaptici = 2*np.random.random((3,1))-1
+
+
+#def sigmond(x):
+#    return 1/(1+np.exp(-x))
+#def sigmond_der(x):
+#    return x*(1-x)
 
