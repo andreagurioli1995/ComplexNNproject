4580b24f2bc1ffe5f0ebc669a1c1d4dceaa88ba0
LFIII <59267254+LFIII@users.noreply.github.com>
2020-11-11 18:00:12 +0100

    cost function 2

--- nn.py
+++ nn.py
@@ -28,14 +28,31 @@ class nn:
     def getPesi(self):
         return self.pesi
 
-    def MSR(self) :
-        
+<<<<<<< HEAD
+    def setOutputs(self, inputs) :
+        for x in range(100) :
+            self.outputs.append(self.feedForward(inputs[x]))
 
+    def MSR(self, targets) :
+        m = 10
+        totalSum = 0
+        # dovremo ricordarci di modificare if x == targets[i] in modo da avere 
+        # ogni volta i valori di una batch successiva, dopo che avremo suddiviso gli input in batches
+        for i in range(m) :
+            targetVector = [float(1) if x == targets[i] else float(0) for x in range(10)]
+            #print(targets[i], targetVector)
+            totalSum = np.linalg.norm(self.outputs[i] - targets[i])**2
+        return totalSum / m
+=======
+    def MSR(self) :
+        pass
+
+>>>>>>> ab82e6f17c2054e1e033dce1d56b4b48b8735b8e
 
     def TrainNN(self, nTimes):
         pass
 
-    def getOutput(self, my_input):
+    def feedForward(self, my_input):
         temp = my_input
         for x in range(len(self.myLayers) + 1) :
             output = self.sigmond(np.dot(self.pesi[x], temp))
@@ -43,31 +60,40 @@ class nn:
         return output
 
 
+nn1 = nn([12, 12])
 
+####################################################
+# input dal file
 
+<<<<<<< HEAD
+targets = []
+inputsTrain = []
+mydataset = open("data/MnistTrain.txt", "r")
+=======
 nn_prova = nn([12, 12])
 # print(x.getPesi())
 
 target = []
 inputT = []
-mydataset = open("data/MnistTrain.txt", "r")
+#mydataset = open("data/MnistTrain.txt", "r")
+mydataset = open(r"C:\\Users\\bigfo\\OneDrive\\Desktop\\dati\\mnistTrain_copy.txt", "r")
+>>>>>>> ab82e6f17c2054e1e033dce1d56b4b48b8735b8e
 for x in range(10000):
-    target.append(int(mydataset.read(1)))
-    inputT.append([int(x) for x in next(mydataset).split()])
+    target = int(mydataset.read(1))
+    number = [int(x) for x in next(mydataset).split()]
+    targets.append(target)
+    inputsTrain.append(number)
 
 mydataset.close()
 
-# print(pesi_sinaptici)
+####################################################
+# feedforward
 
+nn1.feedForward(inputsTrain[0])
+nn1.setOutputs(inputsTrain)
 
-#print(target[0]," ",inputT[0], len(inputT[0]))
+minibatchesSize = 10
+minibatch = inputsTrain[:minibatchesSize]
 
-# a=np.array([inputT[0]]).T
-
-a = np.array(inputT[0])
-
-
-
-print(nn_prova.getOutput(inputT[0]))
-
-# a' = W a
+print("COST: ", nn1.MSR(targets))
+print("\n\n\n\n\n")
